30b25165b7b463ed0a0cd6b0a87580693d61fc93
LoloLavaur <151183853+LoloLavaur@users.noreply.github.com>
2023-11-17 19:02:16 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@ self.addEventListener('install', (event) => {
                 '/sauveur.html',
                 '/style.css',
                 '/manifest.json',
-                'https://via.placeholder.com/150', // Remplace avec les liens réels de tes images
+                'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FOuiEtHop%2F&psig=AOvVaw0UYVUHqRugpsCC5Ek9n_oG&ust=1700330244437000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJiey_PNy4IDFQAAAAAdAAAAABAE', // Remplace avec les liens réels de tes images
             ]);
         })
     );
